0e17d004ee93832192002f595e92185e7dddf600
Documenter.jl <documenter@juliadocs.github.io>
2022-02-27 21:37:38 +0000

    build based on 7492c73

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Wakame","category":"page"},{"location":"#Wakame","page":"Home","title":"Wakame","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Wakame.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Wakame]","category":"page"}]
+}
