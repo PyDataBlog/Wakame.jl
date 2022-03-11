316b84bdc5de0af3ce7099c4ff203f3cefa25b1b
PyDataBlog <pimpfada@gmail.com>
2022-03-12 00:20:15 +0100

    Attempt to run tests

--- src/mecab.jl
+++ src/mecab.jl
@@ -4,7 +4,7 @@ deps = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
 if isfile(deps)
     include(deps)
 else
-    error("MeCab not properly installed. Please run Pkg.build(\"MeCab\")")
+    error("MeCab not properly installed. Please run Pkg.build(\"Wakame\")")
 end
 
 @assert @isdefined libmecab
